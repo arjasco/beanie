e769ede0116864b5696335518269cf1844645cdd
Aaron Costello <aaronjamescos@gmail.com>
2018-09-16 14:53:29 +0100

    Bury command has no delay option

--- src/Commands/BuryCommand.php
+++ src/Commands/BuryCommand.php
@@ -17,23 +17,17 @@ class BuryCommand implements Command
      * @var int
      */
     protected $priority;
-    /**
-     * @var int
-     */
-    protected $delay;
 
     /**
      * Create a new "bury" command.
      *
      * @param int $id
      * @param int $priority
-     * @param int $delay
      */
-    public function __construct($id, $priority, $delay)
+    public function __construct($id, $priority)
     {
         $this->id = $id;
         $this->priority = $priority;
-        $this->delay = $delay;
     }
 
     /**
